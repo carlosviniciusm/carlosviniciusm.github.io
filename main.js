f60f7114d22ba268a393e5bc87a341e35c2fe59e
Carlos Vinicius <carlos.melo@terceiros.uniasselvi.com.br>
2022-12-04 22:21:54 -0300

    Altera código para usar async await

--- main.js
+++ main.js
@@ -1,38 +1,52 @@
 $(document).ready(function() {
     $(document).on('click', '#buttonRequest', function() {
-        getFrase('frase1', '1', '9000')
-        .then(() => {
-            return getFrase('frase2', '2', '5000')
-            .then(() => {
-                return getFrase('frase3', '3', '2000');
-            });
-        })
-        .then(() => {
-            console.log('Eu serei o último a ser exibido');
-        })
+        
+
+        executa();
     });
 });
 
+async function executa() {
+    try {
+        await getFrase('frase1', '1', '9000');
+        await getFrase('frase2', '2', '5000');
+        await getFrase('frase3', '3', '2000');
+    } catch (e) {
+        console.log(e);
+    }
+
+    Swal.close();
+}
+
 function getFrase(id, nr, tempo) {
+    let url = `https://api.adviceslip.com/advice`;
+
     Swal.close();
     Swal.fire({ text: 'Aguarde! Solicitando frase ' + nr + '...' })
-    Swal.showLoading()
-
-    let url = `https://api.adviceslip.com/advice`;
+    Swal.showLoading();
 
     return new Promise((resolve, reject) => {
         setTimeout(() => {
+            if(typeof id !== 'string') {
+                reject('Caiu no erro');
+                return;
+            }
+
             fetch(url)
             .then((response) => response.json())
             .then((data) => {
                 $("#" + id).html(data.slip.advice)
                 $("#" + id).append(' ID - ' + nr);
-                return data;
             })
-            .then((data) => {
-                Swal.close();
-                resolve(data);
+            .then(() => {
+                resolve('Busca de frase concluída com sucesso!');
+                return;
+            })
+            .catch(e => {
+                console.log(e);
+                
             });
-        }, tempo)
+        }, tempo);
+
     });
 }
